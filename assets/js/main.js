b149b18eaeb5d9106a772ec62c01894747a4436e
Nyonjee Zeogar <54722046+NyonjeeZeogar@users.noreply.github.com>
2023-05-02 18:35:50 -0500

    Update Qualifications Section HTML/CSS/JS.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 /*==================== MENU SHOW Y HIDDEN ====================*/
 const navMenu = document.getElementById('nav-menu');
-    navToggle = document.getElementById('nav-toggle');
-    navClose = document.getElementById('nav-close');
+navToggle = document.getElementById('nav-toggle');
+navClose = document.getElementById('nav-close');
 /*===== MENU SHOW =====*/
 /* Validate if constant exists */
 if(navToggle) {
@@ -48,7 +48,26 @@ skillsHeader.forEach((el) => {
 })
 
 /*==================== QUALIFICATION TABS ====================*/
+let tabs, tabsContents;
 
+tabs = document.querySelectorAll('[data-target]');
+tabContents = document.querySelectorAll('[data-content]');
+
+tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+        const target = document.querySelector(tab.dataset.target)
+
+        tabContents.forEach(tabContent => {
+            tabContent.classList.remove('qualification__active')
+        })
+        target.classList.add('qualification__active')
+
+        tabs.forEach(tab => {
+            tab.classList.remove('qualification__active')
+        })
+        tab.classList.add('qualification__active')
+    })
+})
 
 /*==================== SERVICES MODAL ====================*/
 
@@ -62,10 +81,10 @@ skillsHeader.forEach((el) => {
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 
 
-/*==================== CHANGE BACKGROUND HEADER ====================*/ 
+/*==================== CHANGE BACKGROUND HEADER ====================*/
 
 
-/*==================== SHOW SCROLL UP ====================*/ 
+/*==================== SHOW SCROLL UP ====================*/
 
 
 /*==================== DARK LIGHT THEME ====================*/ 
